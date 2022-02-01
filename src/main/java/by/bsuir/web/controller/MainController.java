5b02f7a9fe0db5f52c8401e0cd3d8fddefdee092
Ivan <ivannekrashevich@gmail.com>
2022-02-02 01:05:51 +0300

    added db connection

--- src/main/java/by/bsuir/web/controller/MainController.java
+++ src/main/java/by/bsuir/web/controller/MainController.java
@@ -1,6 +1,9 @@
 package by.bsuir.web.controller;
 
 
+import by.bsuir.web.entity.User;
+import by.bsuir.web.entity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public String homePage(Model model) {
-        model.addAttribute("title", "baldezh");
+        Iterable<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "homePage";
     }
 
